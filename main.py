43da30d83a99e99a59bbdd90f97f584f1e8013ce
Alessandro Capialbi <capialbi.alessandro@studenti-ittfedifermi.edu.it>
2024-08-06 12:17:48 +0200

    Implement downsample function to reduce data volume

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import numpy as np
 import open3d as o3d
 
-filename = "C:\\Users\\aless\\Desktop\\LiDARClusterTrack\\alessandrocapialbi\\LiDAR_Cluster_Track\\filtered_sensors_data\\sensor_3_22.csv"
+filename = "C:\\Users\\aless\\Desktop\\LiDARClusterTrack\\alessandrocapialbi\\LiDAR_Cluster_Track\\filtered_sensors_data\\sensor_3_20.csv"
 
 data = np.genfromtxt(filename, delimiter=',', skip_header=1, usecols=[5, 6, 7])
 
@@ -10,7 +10,8 @@ data = np.genfromtxt(filename, delimiter=',', skip_header=1, usecols=[5, 6, 7])
 pcd = o3d.geometry.PointCloud()
 print("Numero di punti: ", len(data))
 pcd.points = o3d.utility.Vector3dVector(data)
-num_points_before = len(pcd.points)
+pcd = pcd.voxel_down_sample(voxel_size=0.3)
+print("Numero di punti dopo il down sampling: ", len(pcd.points))
 
 
 # Visualizza la nuvola di punti
