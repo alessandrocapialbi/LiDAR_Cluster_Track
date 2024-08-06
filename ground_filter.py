ecf2f1c4670a811c609a7c747611170d41b9f4d6
Alessandro Capialbi <capialbi.alessandro@studenti-ittfedifermi.edu.it>
2024-08-06 11:46:26 +0200

    Filter ground function that saves filtered data from sensors

--- ground_filter.py
+++ ground_filter.py
@@ -1,10 +1,27 @@
-import numpy as np
-import open3d as o3d
+import os
+import pandas as pd
+
+point_cloud_directory = "\\sensors\\"
+
+output_directory = "\\filtered_data_sensors\\"
 
 
 # All the z points below the threshold are considered as ground points.
-def filter_ground(points, z_threshold):
-    filtered_points = points[points[:, 2] > z_threshold]
-    pcd = o3d.geometry.PointCloud()
-    pcd.points = o3d.utility.Vector3dVector(filtered_points)
-    return pcd
+def filter_ground(z_threshold):
+    if not os.path.exists(output_directory):
+        os.makedirs(output_directory)
+
+    for file_name in os.listdir(point_cloud_directory):
+        if file_name.endswith('.csv'):
+            input_file_path = os.path.join(point_cloud_directory, file_name)
+            output_file_path = os.path.join(output_directory, file_name)
+
+            # Read CSV file into a DataFrame
+            df = pd.read_csv(input_file_path)
+
+            # Filter out the ground based on a z_threshold
+            filtered_df = df[df['z'] > z_threshold]
+
+            # Save the filtered DataFrame to a new CSV file
+            filtered_df.to_csv(output_file_path, index=False)
+            print(f'Processed and saved: {output_file_path}')
--- main.py
+++ main.py
@@ -8,19 +8,11 @@ from clustering import clustering_and_bounding_boxes
 
 
 def main():
-    point_cloud_directory = "..\\..\\sensors\\"
-
     # Sensor to use
     selected_indices = select_sensors(5)
 
-    # Load the point clouds from the selected sensors
-    point_clouds = []
-    for i in selected_indices:
-        filenames = load_point_clouds_from_sensors(point_cloud_directory, selected_indices[i], range(20, 31))
-        point_clouds.extend([o3d.io.read_point_cloud(filename) for filename in filenames])
-
     z_threshold = -1.5
-    filtered_point_clouds = [filter_ground(pcd, z_threshold) for pcd in point_clouds]
+    filtered_point_clouds = filter_ground(z_threshold)
 
     o3d_point_clouds = [o3d.geometry.PointCloud(o3d.utility.Vector3dVector(pcd)) for pcd in filtered_point_clouds]
 
