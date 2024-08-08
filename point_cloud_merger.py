2107443eb5c10a9cbc49ffa6c92e320ca99c627f
Alessandro Capialbi <capialbi.alessandro@studenti-ittfedifermi.edu.it>
2024-08-08 16:21:31 +0200

    Function to merge all the transformed point clouds with global coordinates

--- point_cloud_merger.py
+++ point_cloud_merger.py
@@ -1,8 +1,9 @@
 import open3d as o3d
+import numpy as np
 
 
 def merge_point_clouds(point_clouds):
-    merged_pcd = o3d.geometry.PointCloud()
-    for pcd in point_clouds:
-        merged_pcd += pcd
-    return merged_pcd
+    pcd_combined = o3d.geometry.PointCloud()
+    combined_points = np.vstack([np.asarray(pcd.points) for pcd in point_clouds])
+    pcd_combined.points = o3d.utility.Vector3dVector(combined_points)
+    return pcd_combined
